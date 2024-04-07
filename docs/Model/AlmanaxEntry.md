02a7f32e51e02e73d588602edd3456d1462c63f0
dofusdude <stelzo@steado.de>
2024-04-07 21:36:11 +0000

    Update from https://github.com/dofusdude/api-docs/commit/fd0df30b683e2eded93c7124db1fc9f194f643ea

--- docs/Model/AlmanaxEntry.md
+++ docs/Model/AlmanaxEntry.md
@@ -7,5 +7,6 @@ Name | Type | Description | Notes
 **bonus** | [**\OpenAPI\Client\Model\AlmanaxEntryBonus**](AlmanaxEntryBonus.md) |  | [optional]
 **date** | **string** |  | [optional]
 **tribute** | [**\OpenAPI\Client\Model\AlmanaxEntryTribute**](AlmanaxEntryTribute.md) |  | [optional]
+**reward_kamas** | **int** | Amount of Kamas you get as reward for finishing this Almanax quest. | [optional]
 
 [[Back to Model list]](../../README.md#models) [[Back to API list]](../../README.md#endpoints) [[Back to README]](../../README.md)
--- lib/Model/AlmanaxEntry.php
+++ lib/Model/AlmanaxEntry.php
@@ -13,9 +13,9 @@
 /**
  * dofusdude
  *
- * # A project for you - the developer. The all-in-one toolbelt for your next Ankama related project.  ## Client SDKs - [Javascript](https://github.com/dofusdude/dofusdude-js) npm i dofusdude-js --save - [Typescript](https://github.com/dofusdude/dofusdude-ts) npm i dofusdude-ts --save - [Go](https://github.com/dofusdude/dodugo) go get -u github.com/dofusdude/dodugo - [Python](https://github.com/dofusdude/dofusdude-py) pip install dofusdude - [PHP](https://github.com/dofusdude/dofusdude-php)  Everything, including this site, is generated out of the [Docs Repo](https://github.com/dofusdude/api-docs). Consider it the Single Source of Truth. If there is a problem with the SDKs, create an issue there.  Your favorite language is missing? Please let me know!  # Main Features - 🥷 **Seamless Auto-Update** load data in the background when a new Dofus version is released and serving it within 2 minutes with atomic data source switching. No downtime and no effects for the user, just always up-to-date.  - ⚡ **Blazingly Fast** all data in-memory, aggressive caching over short time spans, HTTP/2 multiplexing, written in Go, optimized for low latency, hosted on bare metal in 🇩🇪.  - 📨 **Discord Integration** Ankama related RSS and Almanax feeds to post to Discord servers with advanced features like filters or mentions. Use the endpoints as a dev or the official [Web Client](https://discord.dofusdude.com) as a user.  - 🩸 **Dofus 2 Beta** from stable to bleeding edge by replacing /dofus2 with /dofus2beta.  - 🗣️ **Multilingual** supporting _en_, _fr_, _es_, _pt_ including the dropped languages from the Dofus website _de_ and _it_.  - 🧠 **Search by Relevance** allowing typos in name and description, handled by language specific text analysis and indexing.  - 🕵️ **Complete** actual data from the game including items invisible to the encyclopedia like quest items.  - 🖼️ **HD Images** rendering game assets to high-res images with up to 800x800 px.  ... and much more on the Roadmap on my Discord.   ## Deploy now. Use forever. Everything you see here on this site, you can use now and forever. Updates could introduce new fields, new paths or parameter but never break backwards compatibility.  There is one exception! **The API will _always_ choose being up-to-date over everything else**. So if Ankama decides to drop languages from the game like they did with their website, the API will loose support for them, too.  ## Thank you! I highly welcome everyone on my [Discord](https://discord.gg/3EtHskZD8h) to just talk about projects and use cases or give feedback of any kind.  The servers have a fixed monthly cost to provide very fast responses. If you want to help me keeping them running or simply donate to that cause, consider becoming a [GitHub Sponsor](https://github.com/sponsors/dofusdude).
+ * # A project for you - the developer. The all-in-one toolbelt for your next Ankama related project.  ## Client SDKs - [Javascript](https://github.com/dofusdude/dofusdude-js) `npm i dofusdude-js --save` - [Typescript](https://github.com/dofusdude/dofusdude-ts) `npm i dofusdude-ts --save` - [Go](https://github.com/dofusdude/dodugo) `go get -u github.com/dofusdude/dodugo` - [Python](https://github.com/dofusdude/dofusdude-py) `pip install dofusdude` - [PHP](https://github.com/dofusdude/dofusdude-php) - [Java](https://github.com/dofusdude/dofusdude-java) Maven with GitHub packages setup  Everything, including this site, is generated out of the [Docs Repo](https://github.com/dofusdude/api-docs). Consider it the Single Source of Truth. If there is a problem with the SDKs, create an issue there.  Your favorite language is missing? Please let me know!  # Main Features - 🥷 **Seamless Auto-Update** load data in the background when a new Dofus version is released and serving it within 10 minutes with atomic data source switching. No downtime and no effects for the user, just always up-to-date.  - ⚡ **Blazingly Fast** all data in-memory, aggressive caching over short time spans, HTTP/2 multiplexing, written in Go, optimized for low latency, hosted on bare metal in 🇩🇪.  - 📨 **Discord Integration** Ankama related RSS and Almanax feeds to post to Discord servers with advanced features like filters or mentions. Use the endpoints as a dev or the official [Web Client](https://discord.dofusdude.com) as a user.  - 🩸 **Dofus 2 Beta** from stable to bleeding edge by replacing /dofus2 with /dofus2beta.  - 🗣️ **Multilingual** supporting _en_, _fr_, _es_, _pt_ including the dropped languages from the Dofus website _de_ and _it_.  - 🧠 **Search by Relevance** allowing typos in name and description, handled by language specific text analysis and indexing.  - 🕵️ **Complete** actual data from the game including items invisible to the encyclopedia like quest items.  - 🖼️ **HD Images** rendering game assets to high-res images with up to 800x800 px.  ... and much more on the Roadmap on my [Discord](https://discord.gg/3EtHskZD8h).
  *
- * The version of the OpenAPI document: 0.8.3
+ * The version of the OpenAPI document: 0.9.0
  * Contact: stelzo@steado.de
  * Generated by: https://openapi-generator.tech
  * Generator version: 7.5.0-SNAPSHOT
@@ -60,7 +60,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static $openAPITypes = [
         'bonus' => '\OpenAPI\Client\Model\AlmanaxEntryBonus',
         'date' => 'string',
-        'tribute' => '\OpenAPI\Client\Model\AlmanaxEntryTribute'
+        'tribute' => '\OpenAPI\Client\Model\AlmanaxEntryTribute',
+        'reward_kamas' => 'int'
     ];
 
     /**
@@ -73,7 +74,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static $openAPIFormats = [
         'bonus' => null,
         'date' => null,
-        'tribute' => null
+        'tribute' => null,
+        'reward_kamas' => null
     ];
 
     /**
@@ -84,7 +86,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static array $openAPINullables = [
         'bonus' => false,
         'date' => false,
-        'tribute' => false
+        'tribute' => false,
+        'reward_kamas' => true
     ];
 
     /**
@@ -175,7 +178,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static $attributeMap = [
         'bonus' => 'bonus',
         'date' => 'date',
-        'tribute' => 'tribute'
+        'tribute' => 'tribute',
+        'reward_kamas' => 'reward_kamas'
     ];
 
     /**
@@ -186,7 +190,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static $setters = [
         'bonus' => 'setBonus',
         'date' => 'setDate',
-        'tribute' => 'setTribute'
+        'tribute' => 'setTribute',
+        'reward_kamas' => 'setRewardKamas'
     ];
 
     /**
@@ -197,7 +202,8 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
     protected static $getters = [
         'bonus' => 'getBonus',
         'date' => 'getDate',
-        'tribute' => 'getTribute'
+        'tribute' => 'getTribute',
+        'reward_kamas' => 'getRewardKamas'
     ];
 
     /**
@@ -260,6 +266,7 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
         $this->setIfExists('bonus', $data ?? [], null);
         $this->setIfExists('date', $data ?? [], null);
         $this->setIfExists('tribute', $data ?? [], null);
+        $this->setIfExists('reward_kamas', $data ?? [], null);
     }
 
     /**
@@ -381,6 +388,40 @@ class AlmanaxEntry implements ModelInterface, ArrayAccess, \JsonSerializable
             throw new \InvalidArgumentException('non-nullable tribute cannot be null');
         }
         $this->container['tribute'] = $tribute;
+
+        return $this;
+    }
+
+    /**
+     * Gets reward_kamas
+     *
+     * @return int|null
+     */
+    public function getRewardKamas()
+    {
+        return $this->container['reward_kamas'];
+    }
+
+    /**
+     * Sets reward_kamas
+     *
+     * @param int|null $reward_kamas Amount of Kamas you get as reward for finishing this Almanax quest.
+     *
+     * @return self
+     */
+    public function setRewardKamas($reward_kamas)
+    {
+        if (is_null($reward_kamas)) {
+            array_push($this->openAPINullablesSetToNull, 'reward_kamas');
+        } else {
+            $nullablesSetToNull = $this->getOpenAPINullablesSetToNull();
+            $index = array_search('reward_kamas', $nullablesSetToNull);
+            if ($index !== FALSE) {
+                unset($nullablesSetToNull[$index]);
+                $this->setOpenAPINullablesSetToNull($nullablesSetToNull);
+            }
+        }
+        $this->container['reward_kamas'] = $reward_kamas;
 
         return $this;
     }
